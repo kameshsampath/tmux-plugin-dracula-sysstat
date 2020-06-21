6a278e67a496765af04ca6ee20379fcac7eafb07
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-06-21 21:35:08 +0530

    add cpu and memory

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -110,7 +110,7 @@ main()
 
 	tmux set-option -ga status-right " #($current_dir/mem.sh) "
 
-  tmux set-option -ga status-right "#[fg=${white},bg=${dark_purple}] %a %m/%d %I:%M %p #(date +%Z) "
+  tmux set-option -ga status-right "#[fg=${white},bg=${dark_purple}] %m/%d %I:%M %p #(date +%Z) "
   
   # window tabs 
   tmux set-window-option -g window-status-current-format "#[fg=${white},bg=${dark_purple}] #I #W "
